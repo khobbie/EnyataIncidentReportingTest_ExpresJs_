19899c466f9ae8923a603fa69004b42b4bb99b90
king kobby <ampahkwabena5@gmail.com>
2022-01-19 17:33:40 +0000

    start

---
+++ fm.js
@@ -0,0 +1,13 @@
+const forever = require('forever-monitor');
+
+const child = new forever.Monitor('server.js', {
+    max: 3,
+    silent: false,
+    uid: 'enyata',
+});
+
+child.on('exit', function() {
+    console.log('server.js has exited after 3 restarts');
+});
+
+child.start();
